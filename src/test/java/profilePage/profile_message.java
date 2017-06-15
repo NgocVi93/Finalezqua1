de645d9fbcebb9ac84f0cf9a9aff8b4406b829e0
Pham Ngoc Vi <vipn@ezqua.com>
2017-06-15 15:19:25 +0700

    Done goi text, emoji , Text&emoji trong chat personal

--- src/test/java/profilePage/profile_message.java
+++ src/test/java/profilePage/profile_message.java
@@ -6,14 +6,20 @@ import static org.testng.Assert.assertEquals;
 import com.sun.jna.platform.win32.OaIdl;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.MultiTouchAction;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
-import javafx.scene.layout.Priority;
+
+
 import org.openqa.selenium.*;
 
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 
+import org.openqa.selenium.interactions.touch.TouchActions;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -32,6 +38,7 @@ import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -52,22 +59,23 @@ public class profile_message {
     public void beforeTest() throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
         //Máy ảo
- //       cap.setCapability("appActivity", "ezqua.dfm.com.prototype.activity.LoginActivity");
-   //     cap.setCapability("appWaitActivity", "ezqua.dfm.com.prototype.activity.LoginActivity', ezqua.dfm.com.prototype.activity.LoginActivity");
+        cap.setCapability("appActivity", "ezqua.dfm.com.prototype.activity.LoginActivity");
+        cap.setCapability("appWaitActivity", "ezqua.dfm.com.prototype.activity.LoginActivity', ezqua.dfm.com.prototype.activity.LoginActivity");
 
         cap.setCapability("app", Ezqua);
         // khai báo platform
-        //cap.setCapability(CapabilityType.VERSION, "5.0.0");
+        cap.setCapability(CapabilityType.VERSION, "5.0.0");
         // khai báo deviceName
-       // cap.setCapability("deviceName", "192.168.56.101:5555");dùng
+       cap.setCapability("deviceName", "192.168.56.101:5555");
 
 
-        //Máy thật dùng
+        /*Máy thật dùng
        // cap.setCapability("appActivity", "ezqua.dfm.com.prototype.activity.LoginActivity");
             cap.setCapability("appWaitActivity", "ezqua.dfm.com.prototype.activity.LoginActivity',ezqua.dfm.com.prototype.activity.LoginActivity");
         cap.setCapability(CapabilityType.VERSION, "6.0.1");
         // khai báo deviceName
         cap.setCapability("deviceName", "3300a7537024b357");
+        */
 
          driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
 
@@ -76,7 +84,7 @@ public class profile_message {
 
 
     @Test(priority =1)
-    public void testCheckMessage() throws InterruptedException {
+    public void CheckCaseSendText() throws InterruptedException {
         String currentScreen = driver.currentActivity();
 
         String screenSignIn = driver.currentActivity();
@@ -95,25 +103,20 @@ public class profile_message {
 
 
         //Click allow access photo, media and f
-        driver.findElementById("com.android.packageinstaller:id/permission_allow_button").click();
+ //       driver.findElementById("com.android.packageinstaller:id/permission_allow_button").click();
         //permission_deny_button
         Thread.sleep(4000);
         //Allow access take pictures
-            driver.findElementById("com.android.packageinstaller:id/permission_allow_button").click();
+  //          driver.findElementById("com.android.packageinstaller:id/permission_allow_button").click();
 
         driver.findElementById("ezqua.dfm.com.prototype:id/btnDrawer").click();
         driver.findElementById("ezqua.dfm.com.prototype:id/main_btnPersonal").click();
 
 
-
-
-
         driver.findElementById("ezqua.dfm.com.prototype:id/tvStatus").click();
-        driver.findElementById("ezqua.dfm.com.prototype:id/edtChat").sendKeys("0154335");
+        driver.findElementById("ezqua.dfm.com.prototype:id/edtChat").sendKeys("hello");
 
         driver.findElementById("ezqua.dfm.com.prototype:id/btnSend").click();
-
-
 
         //   driver. navigate().back();
 
@@ -122,38 +125,94 @@ public class profile_message {
 
         for (int i = 0; i < listMessage.size(); i++) {
             String s = listMessage.get(i).getText();
-            if(s!="0154335")
-               i++;
-            else if(s=="0154335")
-            {
-                assertEquals(s,"0154335","Passed");
-            }
+            String expected="hihihehe";
+            System.out.println(s);
 
-            else
+          //  assertEquals(s,"abc");
+            if(s.equals(expected))
             {
-                System.out.println("Failed");
+                System.out.println("Passed "+" tai vi tri "+i+"\n");
             }
+            else
+                System.out.println("Failed "+" vi tri "+i+"\n");
 
 
         }
 
     }
     @Test(priority =2)
+    public void CheckCaseSendEmoji() throws InterruptedException
+    {
+
+        driver.findElementById("ezqua.dfm.com.prototype:id/btnEmoji").click();
+
+    int x = driver.findElement(MobileBy.id("ezqua.dfm.com.prototype:id/emojicon_icon")).getLocation().getX();
+    int y = driver.findElement(MobileBy.id("ezqua.dfm.com.prototype:id/emojicon_icon")).getLocation().getY();
+
+    MultiTouchAction multiTouch = new MultiTouchAction(driver);
+    TouchAction action1 = new TouchAction(driver);
+    TouchAction action2 = new TouchAction(driver);
+    TouchAction action3 = new TouchAction(driver);
+    action1.press(x + 5, y + 5).waitAction(100).release(); //3.5sec for sure, 2 pixels inside el
+    action2.press(x + 300, y + 600).waitAction(1000).release(); //3.5sec for sure, 4 pixels inside el
+
+    multiTouch.add(action1).add(action2).perform();
+    Thread.sleep(4000);
+
+        driver.findElementById("ezqua.dfm.com.prototype:id/btnSend").click();
+
+
+    }
+    @Test(priority =3)
+    public void CheckCaseSendTextAndemoji() throws InterruptedException
+    {
+
+        driver.findElementById("ezqua.dfm.com.prototype:id/tvStatus").click();
+        driver.findElementById("ezqua.dfm.com.prototype:id/edtChat").sendKeys("Send Text & Emoji");
+        driver.findElementById("ezqua.dfm.com.prototype:id/btnSend").click();
+
+        int x = driver.findElement(MobileBy.id("ezqua.dfm.com.prototype:id/emojicon_icon")).getLocation().getX();
+        int y = driver.findElement(MobileBy.id("ezqua.dfm.com.prototype:id/emojicon_icon")).getLocation().getY();
+
+        MultiTouchAction multiTouch = new MultiTouchAction(driver);
+        TouchAction action1 = new TouchAction(driver);
+        TouchAction action2 = new TouchAction(driver);
+        TouchAction action3 = new TouchAction(driver);
+        action1.press(x + 5, y + 5).waitAction(100).release(); //3.5sec for sure, 2 pixels inside el
+        action2.press(x + 300, y + 600).waitAction(1000).release(); //3.5sec for sure, 4 pixels inside el
+
+        multiTouch.add(action1).add(action2).perform();
+        Thread.sleep(4000);
+
+        driver.findElementById("ezqua.dfm.com.prototype:id/btnSend").click();
+
+
+    }
+    /*
+    @Test(priority =2)
     public void addFriends() throws InterruptedException {
         String currentScreen = driver.currentActivity();
 
         driver.navigate().back();
         driver.findElementById("ezqua.dfm.com.prototype:id/fabContactPersonAdd").click();
-        driver.findElementByClassName("android.widget.EditText").sendKeys("0987476321");
-        driver.findElementByClassName("android.widget.Button").click();
+    //    driver.findElementByClassName("android.widget.EditText").sendKeys("0987476321");
+      //  driver.findElementByClassName("android.widget.Button").click();
+        driver.lockDevice();
+        //TouchAction touch=new TouchAction(driver);
+       // touch.press(10,20);
 
+    WebElement element;
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, String> scrollObject = new HashMap<String, String>();
+
+        scrollObject.put("direction", "down");
+       // scrollObject.put("element", ((RemoteWebElement) element).getId("2558"));
+        js.executeScript("mobile: scroll", scrollObject);
 
 
     }
-
-
-
-
+    */
     @AfterTest
     public void afterTest()
     {
